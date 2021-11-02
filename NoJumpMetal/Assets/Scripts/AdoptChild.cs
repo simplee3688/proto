6c1a312accfead50622911ad5563bbeb7b619929
simplee3688 <simplee@cau.ac.kr>
2021-11-03 01:23:41 +0900

    upload
    유니티 파일 업로드

---
+++ NoJumpMetal/Assets/Scripts/AdoptChild.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AdoptChild : MonoBehaviour
+{
+    SpriteRenderer rend;
+    FixedJoint2D fx;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rend = GetComponent<SpriteRenderer>();
+        fx = GetComponent<FixedJoint2D>();
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnCollisionStay2D(Collision2D collision)
+    {
+        if(collision.gameObject.tag == "Player")
+        {
+            rend.color = Color.yellow;
+            if (Input.GetKey(KeyCode.X))
+            {
+                transform.parent = collision.transform;
+                transform.tag = "PlayerAttachment";
+                fx.enabled = true;
+                fx.connectedBody = collision.gameObject.GetComponent<Rigidbody2D>();
+            }
+        }
+        
+    }
+
+    private void OnCollisionExit2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+            rend.color = Color.black;
+        }
+    }
+}
---
+++ NoJumpMetal/Assets/Scripts/MoveHorizontal.cs
@@ -0,0 +1,82 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveHorizontal : MonoBehaviour
+{
+    public float speed;
+    public float fric;
+    public float maxspeed;
+    public float min;
+    
+
+    Rigidbody2D rb;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Z))
+        {
+            Transform[] tp = GetComponentsInChildren<Transform>();
+            foreach (Transform child in tp)
+            {
+                Debug.Log(child.name);
+                if (child.tag == "PlayerAttachment")
+                {
+                    
+
+                    child.tag = "MapObject";
+                    child.transform.parent = null;
+                    child.GetComponent<FixedJoint2D>().enabled = false;
+
+                    
+
+                }
+
+            }
+        }
+
+
+    }
+
+    void FixedUpdate()
+    {
+        //Debug.Log(rb.worldCenterOfMass);
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            rb.angularVelocity += speed;
+            //rb.AddForce(new Vector2(-min, 0));
+        }
+        else if (Input.GetKey(KeyCode.RightArrow))
+        {
+            rb.angularVelocity -= speed;
+            //rb.AddForce(new Vector2(min, 0));
+        }
+        else if (rb.velocity.y == 0)
+        {
+            rb.angularVelocity = rb.angularVelocity * fric;
+        }
+        if (Mathf.Abs(rb.angularVelocity) > maxspeed)
+        {
+            if (rb.angularVelocity > 0)
+            {
+                rb.angularVelocity = maxspeed;
+            }
+            else
+            {
+                rb.angularVelocity = -maxspeed;
+            }
+
+
+        }
+
+        
+    }
+}
